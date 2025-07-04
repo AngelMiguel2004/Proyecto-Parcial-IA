ad27026f24371f8d6c5e04a6f4e633c6c7812d6d
AngelMiguel2004 <154101593+AngelMiguel2004@users.noreply.github.com>
2025-07-04 03:25:50 +0000

    aqui estan los bloques que representanla pared o laberinto del jugador

---
+++ wall.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+from config import *
+
+# Clase WallObject, representa una pared en el laberinto
+class WallObject(pygame.sprite.Sprite):
+    def __init__(self, screen, color, rect):
+        super().__init__()
+        self.screen = screen
+        self.color = color
+        self.rect = rect.copy()
+        self.wallthickness = WALLTHICKNESS
+
+        # Crea una imagen del bloque y la rellena con el color dado
+        self.image = pygame.Surface([rect.width,rect.height])
+        self.image.fill(color)
+
+    def draw(self):
+        # Método de dibujo (no implementado)
+        pass
